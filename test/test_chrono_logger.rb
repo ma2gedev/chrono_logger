40daf70a1837f4386d649f6a9cd2fbc13691ae2f
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-01-23 15:11:18 +0900

    check file line count and file closed

--- test/test_chrono_logger.rb
+++ test/test_chrono_logger.rb
@@ -37,6 +37,7 @@ private
     begin
       Delorean.time_travel_to '2015-01-22 23:59:59.990'
       logger = ChronoLogger.new(@format)
+      old_logdev = logger.instance_variable_get('@logdev').dev
       Parallel.map(['a', 'b'], option) do |letter|
         5000.times do
           logger.info letter * 5000
@@ -44,8 +45,17 @@ private
       end
       assert_no_match(/log shifting failed/, $stderr.string)
       assert_no_match(/log writing failed/, $stderr.string)
+      assert { !old_logdev.closed? }
       assert { File.exist?(old_log) }
       assert { File.exist?(new_log) }
+      assert { File.read(old_log).count("\n") + File.read(new_log).count("\n") == 10000 }
+
+      sleep ChronoLogger::TimeBasedLogDevice::DELAY_SECOND_TO_CLOSE_FILE + 1
+      old_logdev.close if option[:in_processes]
+      assert { old_logdev.closed? } # TODO: check in multiprocess
+      assert { File.exist?(old_log) }
+      assert { File.exist?(new_log) }
+      assert { File.read(old_log).count("\n") + File.read(new_log).count("\n") == 10000 }
     ensure
       $stderr, stderr = stderr, $stderr
       Delorean.back_to_the_present
